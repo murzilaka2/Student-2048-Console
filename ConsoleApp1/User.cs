7687bfd32ac7c449de4bd7f66017dd529d93eda0
Leonid <enykoruna1@gmail.com>
2023-08-10 18:00:33 +0300

    Add project files.

---
+++ ConsoleApp1/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public DateTime DeletedOn { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Id - {Id}, Name - {Name}, Address - {Address}.";
+        }
+    }
+}
